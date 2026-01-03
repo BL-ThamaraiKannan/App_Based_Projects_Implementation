fd47675f33557b9515d3aca514d51b33ca6766ac
BL-ThamaraiKannan <thamarai.kannan@bridgelabz.com>
2026-01-03 12:10:11 +0530

    feat(UC1): Added code to print OOPS acronym to console

--- src/com/bridgelabz/oopsbannerapp/OOPSBannerApp.java
+++ src/com/bridgelabz/oopsbannerapp/OOPSBannerApp.java
@@ -1,9 +1,19 @@
 package com.bridgelabz.oopsbannerapp;
 
+/**
+* 00PSBannerApp UC1 - 00PS Banner Display Application
+*
+* This class demonstrates a simple Java application that displays the Object Oriented Programming System OOPS acronym to the console.
+*
+* @author Thamarai Kannan
+* @version 1.0
+*/
+
 public class OOPSBannerApp {
 
 	public static void main(String[] args) {
-
+		
+		System.out.println("OOPS");
 	}
 
 }

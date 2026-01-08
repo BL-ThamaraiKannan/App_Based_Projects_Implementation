f6b6185128b02c452ea6af8b9214e5b228862e41
BL-ThamaraiKannan <thamarai.kannan@bridgelabz.com>
2026-01-08 17:34:43 +0530

    Added code to develop a Factory Robot Hazard Analyzer system

---
+++ src/com/bridgelabz/factoryrobothazardanalyzer/RobotSafetyException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.factoryrobothazardanalyzer;
+
+//Custom Exception Class
+class RobotSafetyException extends Exception {
+public RobotSafetyException(String message) {
+   super(message);
+}
+}
